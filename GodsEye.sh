1301c71d8d8de4d546487fc05c1318678601d64b
0secdevil <54764832+0secdevil@users.noreply.github.com>
2019-09-04 13:00:25 +0530

    GodsEye

--- GodsEye.sh
+++ GodsEye.sh
@@ -144,7 +144,7 @@ printf "\n"
 printf "\e[1;93m       .:.:.\e[0m\e[1;77m Phishing Tool coded by:  @0secdevil \e[0m\e[1;93m.:.:.\e[0m\n"
 printf "\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
-printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by ShellPhish ::\e[0m\n"
+printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by GodsEye ::\e[0m\n"
 printf "\n"
 }
 
